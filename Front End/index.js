625868b3dce4df7939c481beb54681427fbe38e8
mharkati1 <45287408+mharkati1@users.noreply.github.com>
2024-01-30 17:29:41 -0500

    Edited some paragraph stuff
    I have also deleted the js file due to it not being needed for the  front-end

--- Front End/index.js
+++
@@ -1,9 +0,0 @@
-
-// Function to calculate Loan Payment on the website 
-function calculateLoanPayment() {
-  const loanAmount = document.getElementById("loan-amount").value;
-  const interestRate = document.getElementById("interest-rate").value / 100 / 12;
-  const loanTerm = document.getElementById("loan-term").value;
-  const monthlyPayment = (loanAmount * interestRate * Math.pow(1 + interestRate, loanTerm)) / (Math.pow(1 + interestRate, loanTerm) - 1);
-  document.getElementById("monthly-payment").innerHTML = "Monthly Payment: $" + monthlyPayment.toFixed(2);
-}
